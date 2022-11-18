a2dcb4e9e5ca2c88bc4118ef7d10f64020ebe013
이소영 <th42500@naver.com>
2022-11-19 00:12:57 +0900

    [Feat] ArticleController - 게시글 등록 기능 추가
    요청 데이터가 잘 넘어오는지 log 출력

--- 이소영/api/src/main/java/com/springboot/api/controller/ArticleController.java
+++ 이소영/api/src/main/java/com/springboot/api/controller/ArticleController.java
@@ -1,15 +1,36 @@
 package com.springboot.api.controller;
 
+import com.springboot.api.domain.dto.ArticleRequestDto;
+import com.springboot.api.domain.entity.Article;
+import com.springboot.api.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 @RequestMapping("/articles")
 public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @GetMapping("/new")
     public String registerPage() {
         return "articles/register";
+    }
+
+    @PostMapping()
+    public String register(ArticleRequestDto articleRequestDto) {
+        log.info(articleRequestDto.toString());
+        Article savedArticle = articleRepository.save(articleRequestDto.toEntity());
+        log.info("generatedId : {}", savedArticle.getId());
+        return "";
     }
 }
