ddd7aafa1f9b0fc556745f5723ab8a4bbb787650
이소영 <th42500@naver.com>
2022-11-19 00:10:00 +0900

    [Feat] Article Entity 생성

---
+++ 이소영/api/src/main/java/com/springboot/api/domain/entity/Article.java
@@ -0,0 +1,27 @@
+package com.springboot.api.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "article")
+@NoArgsConstructor
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
