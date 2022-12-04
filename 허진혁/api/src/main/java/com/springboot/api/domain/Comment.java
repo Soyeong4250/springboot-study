3b307fbb58d4191af83fadcdbfff967cb04f6185
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-04 16:03:59 +0900

    [Feat] 댓글, 병원, 유저 엔티티 추가

---
+++ 허진혁/api/src/main/java/com/springboot/api/domain/Comment.java
@@ -0,0 +1,27 @@
+package com.springboot.api.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "comments")
+public class Comment extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
