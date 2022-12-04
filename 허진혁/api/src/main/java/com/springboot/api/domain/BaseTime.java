b6e80e9022d748b83331f702fc9f4949bb4a9ea0
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-04 16:03:18 +0900

    [Refactor, Feat] 1. Period -> BaseTime 2. 작성일, 수정일 추,

--- 허진혁/api/src/main/java/com/springboot/api/domain/BaseTime.java
+++ 허진혁/api/src/main/java/com/springboot/api/domain/BaseTime.java
@@ -3,16 +3,17 @@ package com.springboot.api.domain;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDate;
 
 @Getter
 @MappedSuperclass
-@Embeddable
-public class Period {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
 
     @Column(name = "created_date")
     @CreatedDate
--- 허진혁/api/src/main/java/com/springboot/api/domain/Post.java
+++ 허진혁/api/src/main/java/com/springboot/api/domain/Post.java
@@ -10,34 +10,35 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Post extends Period{
+@Table(name = "Posts")
+public class Post extends BaseTime {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
     private Long id;
 
     @Column(length = 100, nullable = false)
     private String title;
-    @Column(length = 500, nullable = false)
+
+    @Column(length = 500, columnDefinition = "TEXT", nullable = false)
     private String content;
+
     @Column(nullable = false)
     private String writer;
-    @Column(nullable = false)
+
+    @Column(columnDefinition = "INTEGER",nullable = false)
     private Integer viewCount;
 
-    @Embedded
-    private Period period;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     @Builder
-    public Post(Long id, String title, String content, String writer, Integer viewCount, Period period) {
+    public Post(Long id, String title, String content, String writer, Integer viewCount) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.writer = writer;
         this.viewCount = viewCount;
-        this.period = period;
     }
 }
