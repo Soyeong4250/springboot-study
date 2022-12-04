ba9e3b90f500dfbc77a3f8e15221992ce36e97dd
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2022-12-04 22:52:48 +0900

    Delete 이소영 directory

--- 이소영/api/src/main/java/com/springboot/api/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.api.repository;
-
-import com.springboot.api.domain.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long>{
-
-}
